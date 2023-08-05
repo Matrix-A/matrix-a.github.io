7359c20213bdabb3517f9ac8b844d37d7f18c2c0
Matrix-A <51079060+Matrix-A@users.noreply.github.com>
2023-08-05 21:21:27 +0800

    site

---
+++ public/js/all.1dd1911a36a3b06cf28ceb62b9872f35167eef5026d106545035d8d7fed29f6c.js
@@ -0,0 +1,6 @@
+const easterEgg = new Konami(() => {
+    var elements = document.getElementsByClassName("hide");
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].style.display = window.getComputedStyle(elements[i])["display"] === "none" ? "initial" : "none";
+    }
+  })
