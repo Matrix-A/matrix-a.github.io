7ed16d606c4e6d1999a7fc33c10f11203d13f443
Matrix-A <51079060+Matrix-A@users.noreply.github.com>
2023-09-08 22:03:36 +0800

    update

--- public/js/all.1dd1911a36a3b06cf28ceb62b9872f35167eef5026d106545035d8d7fed29f6c.js
+++
@@ -1,6 +0,0 @@
-const easterEgg = new Konami(() => {
-    var elements = document.getElementsByClassName("hide");
-    for (var i = 0; i < elements.length; i++) {
-        elements[i].style.display = window.getComputedStyle(elements[i])["display"] === "none" ? "initial" : "none";
-    }
-  })
--- public/js/all.98adf9d357e5276e31ad6f095919a2086c3a9f6347bd1fc6fc27b657cb52b491.js
+++
@@ -1,6 +0,0 @@
-const easterEgg = new Konami(() => {
-    var elements = document.getElementsByClassName("hide");
-    for (var i = 0; i < elements.length; i++) {
-        elements[i].style.display = elements[i].style.display === "none" ? "initial" : "none";
-    }
-  })
